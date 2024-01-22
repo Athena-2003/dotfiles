6ec17c09f55f9c12110514b9799518f767483085
Shashank B N <117376750+Athena-2003@users.noreply.github.com>
2024-01-22 18:27:30 +0530

    Add files via upload

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.font = wezterm.font { family = 'JetBrainsMono Nerd Font' , weight = 'Medium', stretch = 'Normal' }
+-- config.color_scheme = 'Gruvbox dark, hard (base16)'
 config.color_scheme = 'rose-pine'
 
 --font size
@@ -13,10 +14,47 @@ config.inactive_pane_hsb = {
   saturation = 0.9,
   brightness = 0.8,
 }
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.7
 
 config.colors = {
   background = "#000000"
 }
+
+config.enable_wayland = true
+
+-- config.colors = {
+--     background = "#000000",
+--     foreground = "#ffffff",
+--     cursor_bg = "#ffffff",
+--     cursor_border = "#ffffff",
+--     selection_bg = "#ffffff",
+--     selection_fg = "#101010",
+--     ansi = {
+--       "#101010",   -- black
+--       "#f5a191",   -- red
+--       "#90b99f",   -- green
+--       "#e6b99d",   -- yellow
+--       "#aca1cf",   -- blue
+--       "#ea83a5",   -- magenta
+--       "#e29eca",   -- cyan
+--       "#A0A0A0",   -- white
+--     },
+--     brights = {
+--       "#282828",   -- bright black
+--       "#ff8080",   -- bright red
+--       "#99FFE4",   -- bright green
+--       "#FFC799",   -- bright yellow
+--       "#b9aeda",   -- bright blue
+--       "#ecaad6",   -- bright magenta
+--       "#f591b2",   -- bright cyan
+--       "#ffffff",   -- bright white
+--     },
+--     cursor = "#ffffff",
+--     cursor_text = "#101010",
+--     scrollbar = "#A0A0A0",
+--     split = "#A0A0A0",
+--     url_color = "#aca1cf",
+--   }
+
 
 return config
