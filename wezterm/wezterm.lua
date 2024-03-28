b05823be195a299e579a2fd32b35ad1c22a6d811
Shashank B N <117376750+Athena-2003@users.noreply.github.com>
2024-03-28 22:31:09 +0530

    Add files via upload

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,13 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.font = wezterm.font { family = 'JetBrainsMono Nerd Font' , weight = 'Medium', stretch = 'Normal' }
+config.font = wezterm.font { family = 'FiraCode Nerd Font' , weight = 'Regular', stretch = 'Normal' }
 -- config.color_scheme = 'Gruvbox dark, hard (base16)'
-config.color_scheme = 'rose-pine'
+-- config.color_scheme = 'rose-pine'
+config.color_scheme = 'Solarized (dark) (terminal.sexy)'
 
 --font size
-config.font_size = 13.0
+config.font_size = 12.0
 config.enable_tab_bar = false
 config.use_fancy_tab_bar=false
 config.tab_bar_at_bottom=true
@@ -14,13 +15,13 @@ config.inactive_pane_hsb = {
   saturation = 0.9,
   brightness = 0.8,
 }
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.9
 
 config.colors = {
-  background = "#000000"
+  background = "#001419"
 }
 
-config.enable_wayland = true
+-- config.enable_wayland = true
 
 -- config.colors = {
 --     background = "#000000",
